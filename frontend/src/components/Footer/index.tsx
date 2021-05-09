7f93072737e7b31aacfcba0f832e3b9856abeb9a
victtor hugo <hgloureirocosta@gmail.com>
2021-05-09 10:26:13 -0300

    Static charts

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+   return (
+      <footer className="footer mt-auto py-3 bg-dark">
+         <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://github.com/Vittorugo/DSVendas" target="_blank" rel="noreferrer">Victtor Hugo</a></p>
+            <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/_vittorugo_/" target="_blank" rel="noreferrer">@_vittorugo_</a></small></p>
+         </div>
+      </footer>
+   );
+}
